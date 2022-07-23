fc70a5e3300ad4adc10edb1598fdc26472598a48
chenbiao <994700146@qq.com>
2022-07-23 16:17:22 +0800

    demo

---
+++ src/main/java/com/mybatisplus/demo/Demo.java
@@ -0,0 +1,16 @@
+package com.mybatisplus.demo;
+
+/**
+ * @author chenbiao
+ * @Data 2022/7/23 12:36 PM
+ */
+public class Demo {
+
+    private int num=1;
+
+    public int add(){
+        num=num+2;
+        return num;
+    }
+
+}
