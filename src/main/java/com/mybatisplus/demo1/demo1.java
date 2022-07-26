0f53c4aa013890b62ffe0c44190e6720298c4ac9
chenbiao <994700146@qq.com>
2022-07-26 16:21:07 +0800

    demo

--- src/main/java/com/mybatisplus/demo1/demo1.java
+++ src/main/java/com/mybatisplus/demo1/demo1.java
@@ -8,11 +8,18 @@ public class demo1 {
 
     public static void main(String[] args) {
 
+        System.out.println(test1());
     }
 
-    public void test1(){
-        int i=3;
+    public static String test1(){
+        String s="hello";
+        try {
+            return s;
+        }finally {
+            s="cb";
+        }
     }
+
 
 
 
